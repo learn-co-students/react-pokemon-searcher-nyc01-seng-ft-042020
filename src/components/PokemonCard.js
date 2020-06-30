64d4d593ff567c4a035bacd184711562e90e4dbb
Srishti Nath <srishti.nath@gmail.com>
2020-06-30 16:13:45 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,34 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  state = {
+    frontImageShow: true
+  }
+
+  togglePicture = () => {
+    this.setState({
+      frontImageShow: !this.state.frontImageShow
+    })
+  }
+  
   render() {
+    let { name, hp } = this.props.pokemon
+    let front = this.props.pokemon.sprites.front
+    let back = this.props.pokemon.sprites.back
     return (
       <Card>
         <div>
-          <div className="image">
-            <img alt="oh no!" />
+          <div className="image" onClick={this.togglePicture}>
+            <img alt="oh no!" src={ this.state.frontImageShow ? front : back }
+             />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{ name }</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              { hp } hp
             </span>
           </div>
         </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,11 +2,15 @@ import React from 'react'
 import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
-class PokemonCollection extends React.Component {
+class PokemonCollection extends React.Component  {
+
   render() {
+    let getAllPokemon = this.props.pokemon.map((onePokemon) => {
+      return <PokemonCard pokemon={onePokemon} key={onePokemon.id} />
+    })
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        {getAllPokemon}
       </Card.Group>
     )
   }
